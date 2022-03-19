be1ab387431833e9a25b82523d5e2ee86fae84d7
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2022-03-19 19:22:19 +0530

    Feat : Added Header Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react"
-
-
+import {Header} from "./Components/Header/Header.js"
 export default function App()
 {
 
     return <div>
-        <h1>I am good</h1>
+        <Header />
     </div>
 }
