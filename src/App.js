e67ee2ac10038f4889af7ac4d9d9a8fa006379c0
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2022-03-20 16:08:30 +0530

    Fix : remove border from the Wish list button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Header } from './Components/Header/Header.js';
-import { Main } from './Components/Main/Main.js';
-import { Footer } from './Components/Footer/Footer.js';
+import HomePage from './Pages/HomePage';
+
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <HomePage/>
+    
   );
 }
