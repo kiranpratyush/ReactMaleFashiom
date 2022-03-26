ab7fe5b9fb46a011ffeac94162e3f1017ad9092e
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2022-03-26 17:37:44 +0530

    Feat:Added Routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Routes,Route} from "react-router-dom"
 import HomePage from "./Pages/HomePage"
 import ShopPage from './Pages/ShopPage';
+import { CartPage } from './Pages/CartPage';
+import { WishListPage } from './Pages/WishListPage';
 export default function App() {
 
   return (
@@ -9,7 +11,8 @@ export default function App() {
     <Routes>
       <Route path ="/" element ={<HomePage/>}></Route>
       <Route path ="/shop" element ={<ShopPage/>}></Route>
-    
+      <Route  path="/cart" element ={<CartPage />}></Route>
+      <Route path ="/wishlist" element ={<WishListPage/>}></Route>
     </Routes>
   )
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../Assets/logo.png';
+import {NavLink} from "react-router-dom"
 import './Header.css';
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
@@ -20,20 +21,20 @@ export function Header() {
             <img src={logo} alt="Male Fashion Logo" />
           </div>
           <nav className="first-nav">
-            <a href="https://www.google.com" class="btn-link">
+            <NavLink to="/" className="btn-link">
               Home
-            </a>
-            <a href="https://www.google.com">Shop</a>
+            </NavLink>
+            <NavLink to="/shop" className='btn-link'>Shop</NavLink>
           </nav>
           <nav className="second-nav">
-            <button className="cart__button">
+            <NavLink to ="/cart" className="cart__button">
               <LocalMallOutlinedIcon />
               <span>{state.data.length}</span>
-            </button>
-            <button className="cart__button">
+            </NavLink>
+            <NavLink to ="/wishlist"className="cart__button">
               <FavoriteBorderOutlinedIcon />
               <span>{state.wishList.length}</span>
-            </button>
+            </NavLink>
           </nav>
         </div>
       </header>
