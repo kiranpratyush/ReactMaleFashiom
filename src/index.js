4bbc9dc86c6f0ccbe48064eed5dfe1429f14a6a5
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2022-03-26 16:32:26 +0530

    Added Browser Router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ContextProvider } from './Contexts/DataContext';
 import { CartContextProvider } from './Contexts/CartContext';
-import { BrowserRouter } from 'react-router-dom';
 
 const div = document.querySelector('#root');
 
@@ -11,9 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <CartContextProvider>
       <ContextProvider>
-        <BrowserRouter>
           <App />
-        </BrowserRouter>
       </ContextProvider>
     </CartContextProvider>
   </React.StrictMode>,
