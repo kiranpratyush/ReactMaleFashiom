81d7af01e281997433892cc482409b7339d10f32
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2022-03-28 14:16:20 +0530

    Fix:Fixed unused imports

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FilterByRating } from '../FilterByRating/FilterByRating';
 import './style.css';
 import { useDataContext } from '../../Contexts/DataContext';
