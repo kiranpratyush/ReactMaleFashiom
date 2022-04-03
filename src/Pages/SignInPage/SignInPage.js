c7da9500c800ae053fbd306dfaa140d5aa1cc835
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2022-04-03 16:38:28 +0530

    Fix:Ignored some unused files

--- src/Pages/SignInPage/SignInPage.js
+++ src/Pages/SignInPage/SignInPage.js
@@ -1,17 +1,17 @@
-import React from "react"
-import { SignIn } from "../../Components/SignIn/SignIn"
-import { SignUp } from "../../Components/SignUp/SignUp"
-import "./SignInpage.css"
+import React from 'react';
+import { SignIn } from '../../Components/SignIn/SignIn';
+import { SignUp } from '../../Components/SignUp/SignUp';
+import { Header } from '../../Components/Header/Header';
+import './SignInpage.css';
 
-
-export function SignInPage()
-{
-    return (
-        <main className="auth-main">
-            <SignIn/>
-            <SignUp/>
-
-
-        </main>
-    )
+export function SignInPage() {
+  return (
+    <>
+      <Header />
+      <main className="auth-main">
+        <SignIn />
+        <SignUp />
+      </main>
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ContextProvider } from './Contexts/DataContext';
+import { CartContextProvider } from './Contexts/CartContext';
 
 const div = document.querySelector('#root');
 
-ReactDOM.render(<App />,div);
+ReactDOM.render(
+  <React.StrictMode>
+    <CartContextProvider>
+      <ContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ContextProvider>
+    </CartContextProvider>
+  </React.StrictMode>,
+  div
+);
